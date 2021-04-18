5019a550b7c867462b1b79d572adce55b7012803
Bhavesh <bhaveshjat9950@gmail.com>
2021-04-18 11:02:53 +0530

    Client side Initialised

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.png";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container maxwidth="lg">
+		<AppBar position="static" color="inherit">
+			<Typography varient="h2" align="center">Mermories</Typography>
+				<img src={memories} alt="mermories" height="60" />
+		</AppBar>
+		<Grow in>
+			<Container>
+				
+				<Grid container justify="space-between" alignItems="stretch" spacein>
+					<Grid item xs={12} sm={7}>
+						<Posts />
+					</Grid>
+					<Grid item xs={12} sm={7}>
+						<Form />
+					</Grid>
+				</Grid>
+			</Container>
+		</Grow>
+    </Container>
+  )
 }
-
-export default App;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+	"mongodb+srv://React:React@mern1.qkrpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.port || 5000;
+
+mongoose
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+	)
+	.catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
